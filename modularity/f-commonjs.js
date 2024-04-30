aecc0d1e54b538f94588d9884c050f2ddd696031
Ervin Mamutov <ervin.news@gmail.com>
2024-04-30 06:30:43 +0200

    fix

--- modularity/f-commonjs.js
+++ modularity/f-commonjs.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const fs = require('node:fs');
+const fs = require('node:fs').promises;
 const vm = require('node:vm');
 
 const RUN_OPTIONS = { timeout: 5000, displayErrors: true };
@@ -9,13 +9,13 @@ const pseudoRequire = (name) => {
 };
 
 const load = async (filePath, sandbox) => {
-  const scr = await fs.readFile(filePath, 'utf-8');
-  const code = `(require, module, __fileName, __dirName) => {\n${src}\n}`;
+  const src = await fs.readFile(filePath, 'utf-8');
+  const code = `(require, module, __filename, __dirname) => {\n${src}\n}`;
   const script = new vm.Script(code);
   const context = vm.createContext(Object.freeze({ ...sandbox }));
   const wrapper = script.runInContext(context, RUN_OPTIONS);
   const module = {};
-  wrapper(pseudoRequire, module, filePath, __dirName);
+  wrapper(pseudoRequire, module, filePath, __dirname);
   return module.exports;
 };
 
