b39c8fa4a78046f8873c6e19cf1a9a438f0923d8
Ervin Mamutov <ervin.news@gmail.com>
2024-05-01 06:44:19 +0200

    added DDD examples

---
+++ DDD-master/JavaScript/5-agnostic/user.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const db = require('./db.js');
+const hash = require('./hash.js');
+
+const users = db('users');
+
+module.exports = {
+  async read(id) {
+    return await users.read(id, ['id', 'login']);
+  },
+
+  async create({ login, password }) {
+    const passwordHash = await hash(password);
+    return await users.create({ login, password: passwordHash });
+  },
+
+  async update(id, { login, password }) {
+    const passwordHash = await hash(password);
+    return await users.update(id, { login, password: passwordHash });
+  },
+
+  async delete(id) {
+    return await users.delete(id);
+  },
+};
